ee06c49161cf32ed644dcd60d59826a5b5496faa
Soumya Veer <veer.soumya@gmail.com>
2017-08-02 12:13:30 -0700

    add greet and display_menu methods to Launcher

--- lib/book_deals/launcher.rb
+++ lib/book_deals/launcher.rb
@@ -7,8 +7,23 @@ module BookDeals
     end
 
     def start
-      greet_user
+      self.greet_user
       display_menu
+    end
+
+    def greet_user
+      self.input_output.say "Welcome to Book Deals"
+      self.input_output.say "----------------------"
+    end
+
+    def self.display_menu
+      self.input_output.say "Select the Category for the deals:"
+      self.input_output.say "1. All Deals (Press 1 to see All deals)"
+      self.input_output.say "2. Best Sellers (Press 2 to see Best Sellers)"
+      self.input_output.say "3. Fiction (Press 3 to see Fiction)"
+      self.input_output.say "4. Biographies (Press 4 to see Biographies)"
+      self.input_output.say "5. Technology (Press 5 to see Technology)"
+      self.input_output.say "6. Young Adults (Press 6 to see Young Adults)"
     end
   end
 end
