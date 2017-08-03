ec27dc35054a69c356c660c697ab907183b75ebf
Soumya Veer <veer.soumya@gmail.com>
2017-08-02 22:43:15 -0700

    fix cli_spec and eliminate monkey_patching code from spec_helper

---
+++ spec/book_deals/cli_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe BookDeals::CLI do
+  let(:cli) { BookDeals::CLI.new }
+
+  describe "say" do
+    it "displays the output to the user" do
+      true
+    end
+  end
+end
