5fb407c89644dff46f4c6a5d5b86077067777fe1
Soumya Veer <veer.soumya@gmail.com>
2017-08-03 14:07:57 -0700

    add get_page method to Scraper

--- lib/book_deals/scraper.rb
+++ lib/book_deals/scraper.rb
@@ -4,6 +4,10 @@ require "pry"
 
 module BookDeals
   class Scraper
+    BOOK_DEALS_URL = "https://www.goodreads.com/ebook-deals"
 
+    def get_page
+      Nokogiri::HTML(open(BOOK_DEALS_URL))
+    end
   end
 end
