d202af8bbfc9f2d3d2843ee3f8578883e6c16bc0
Soumya Veer <veer.soumya@gmail.com>
2017-08-05 14:32:34 -0700

    fix scrape_categories_from_home_page method and add scrape_deals_from_category_page method

--- lib/book_deals/scraper.rb
+++ lib/book_deals/scraper.rb
@@ -15,24 +15,17 @@ module BookDeals
       category_html_elements = self.get_page.css(".gr-dealsCategoryExplorer .u-unstyledListItem li a")
       category_html_elements.map do |html_element|
         {
-         name: html_element.text,
-         category_url: HOME_URL + html_element.attr("href")
+          name: html_element.text,
+          category_url: HOME_URL + html_element.attr("href")
         }
       end
     end
 
-    def scrape_deals_from_category_page(category)
+    def scrape_deals_from_category_page
       #1. scrape deals in books from category page
-      #class = "gr-deal"
-      #  class = '.gr-mediaBox__desc'
-      #    class = '.gr-book__title'
-      #      href = show link for book -element a
-      #  class="gr-book__author " ,element - a
-      #  class="gr-book__additionalContent"
-      #    categories - class="gr-deal__tag"
-      #    description - class = "gr-deal__blurb" - element span
-      #    price - class="deal__price__deal"
-      #    original_price - class="gr-deal__price__original"
+      deals_html_doc = Nokogiri::HTML(open("https://www.goodreads.com/ebook-deals/today-only-book-deals"))
+      deals_html_elements = deals_html_doc.css(".gr-deals .visibleWide")
       end
+    end
   end
 end
