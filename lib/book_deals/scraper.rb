a11a3c8a71350794850f0a4674d92631ff3005e3
Soumya Veer <veer.soumya@gmail.com>
2017-08-05 14:43:12 -0700

    add constants to Scraper

--- lib/book_deals/scraper.rb
+++ lib/book_deals/scraper.rb
@@ -6,13 +6,22 @@ module BookDeals
   class Scraper
     HOME_URL = "https://www.goodreads.com"
     BOOK_DEALS_URL = HOME_URL + "ebook-deals"
+    HOME_PAGE_HTML_ELEMENTS = ".gr-dealsCategoryExplorer .u-unstyledListItem li a"
+    CATEGORY_PAGE_HTML_ELEMENTS = ".gr-deals .visibleWide"
+    DEALS_HTML_ELEMENT_TITLE = ".gr-book__title"
+    DEALS_HTML_ELEMENT_AUTHOR = ".gr-book__author a"
+    DEALS_HTML_ELEMENT_CATEGORIES = ".gr-deal__tag"
+    DEALS_HTML_ELEMENT_DESCRIPTION = ".gr-deal__blurb"
+    DEALS_HTML_ELEMENT_DEAL_PRICE = ".gr-deal__price__deal"
+    DEALS_HTML_ELEMENT_ORIGINAL_PRICE = ".gr-deal__price__original strike"
+    DEALS_HTML_ELEMENT_DATETIME = ".gr-deal__expirationDate"
 
     def get_page
       Nokogiri::HTML(open(BOOK_DEALS_URL))
     end
 
     def scrape_categories_from_home_page
-      category_html_elements = self.get_page.css(".gr-dealsCategoryExplorer .u-unstyledListItem li a")
+      category_html_elements = self.get_page.css(HOME_PAGE_HTML_ELEMENTS)
       category_html_elements.map do |html_element|
         {
           name: html_element.text,
@@ -22,19 +31,18 @@ module BookDeals
     end
 
     def scrape_deals_from_category_page
-      #1. scrape deals in books from category page
       deals_html_doc = Nokogiri::HTML(open("https://www.goodreads.com/ebook-deals/today-only-book-deals"))
-      deals_html_elements = deals_html_doc.css(".gr-deals .visibleWide")
+      deals_html_elements = deals_html_doc.css(CATEGORY_PAGE_HTML_ELEMENTS)
 
       deals_html_elements.map do |html_element|
         {
-          title: html_element.css(".gr-book__title").text,
-          author: html_element.css(".gr-book__author a").text,
-          categories: html_element.css(".gr-deal__tag").text,
-          description: html_element.css(".gr-deal__blurb").text,
-          deal_price: html_element.css(".gr-deal__price__deal").text,
-          original_price: html_element.css(".gr-deal__price__original strike").text,
-          deal_expiration_datetime: html_element.css(".gr-deal__expirationDate").text.split(" ").drop(1)
+          title: html_element.css(DEALS_HTML_ELEMENT_TITLE).text,
+          author: html_element.css(DEALS_HTML_ELEMENT_AUTHOR).text,
+          categories: html_element.css(DEALS_HTML_ELEMENT_CATEGORIES).text,
+          description: html_element.css(DEALS_HTML_ELEMENT_DESCRIPTION).text,
+          deal_price: html_element.css(DEALS_HTML_ELEMENT_DEAL_PRICE).text,
+          original_price: html_element.css(DEALS_HTML_ELEMENT_ORIGINAL_PRICE).text,
+          deal_expiration_datetime: html_element.css(DEALS_HTML_ELEMENT_DATETIME).text.split(" ").drop(1)
         }
       end
     end
