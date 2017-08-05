3a21335d78f39e6f6781b26c9dc1a0852fb1632a
Soumya Veer <veer.soumya@gmail.com>
2017-08-05 14:33:41 -0700

    scrape deals from category url in Scraper

--- lib/book_deals/scraper.rb
+++ lib/book_deals/scraper.rb
@@ -25,6 +25,17 @@ module BookDeals
       #1. scrape deals in books from category page
       deals_html_doc = Nokogiri::HTML(open("https://www.goodreads.com/ebook-deals/today-only-book-deals"))
       deals_html_elements = deals_html_doc.css(".gr-deals .visibleWide")
+
+      deals_html_elements.map do |html_element|
+        {
+          title: html_element.css(".gr-book__title").text,
+          author: html_element.css(".gr-book__author a").text,
+          categories: html_element.css(".gr-deal__tag").text,
+          description: html_element.css(".gr-deal__blurb").text,
+          deal_price: html_element.css(".gr-deal__price__deal").text,
+          original_price: html_element.css(".gr-deal__price__original strike").text,
+          deal_expiration_datetime: html_element.css(".gr-deal__expirationDate").text.split(" ").drop(1)
+        }
       end
     end
   end
