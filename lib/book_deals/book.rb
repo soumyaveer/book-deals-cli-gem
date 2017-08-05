abe8f24ce037a603b72622e576acf0ba9859b4bb
Soumya Veer <veer.soumya@gmail.com>
2017-08-05 16:58:21 -0700

    add categories for book

--- lib/book_deals/book.rb
+++ lib/book_deals/book.rb
@@ -1,5 +1,11 @@
 module BookDeals
   class Book
-    attr_accessor :title, :author, :description
+    attr_accessor :title, :author, :description, :deal, :categories
+
+    def initialize
+      @categories = []
+    end
+
+    
   end
 end
