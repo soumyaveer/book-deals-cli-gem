0648e0cb047bcba34b8173ef42a26eb086cbff9c
Soumya Veer <veer.soumya@gmail.com>
2017-08-05 18:42:02 -0700

    construct models from scraped data

--- lib/book_deals/scraper.rb
+++ lib/book_deals/scraper.rb
@@ -3,6 +3,8 @@ require "open-uri"
 require "pry"
 
 module BookDeals
+  attr_accessor :category
+
   class Scraper
     HOME_URL = "https://www.goodreads.com"
     BOOK_DEALS_URL = HOME_URL + "ebook-deals"
@@ -20,32 +22,32 @@ module BookDeals
       Nokogiri::HTML(open(BOOK_DEALS_URL))
     end
 
-    # TODO: convert to objects instead of hash
     def scrape_categories_from_home_page
       category_html_elements = self.get_page.css(HOME_PAGE_HTML_ELEMENTS)
-      category_html_elements.map do |html_element|
-        {
-          name: html_element.text,
-          category_url: HOME_URL + html_element.attr("href")
-        }
+      category_html_elements.each do |html_element|
+        unless html_element.text == "Recommended for You"
+          self.category = Category.new
+          self.category.name =  html_element.text
+          self.category.url =  HOME_URL + html_element.attr("href")
+        end
       end
     end
 
-    # TODO: convert to objects instead of hash
-    def scrape_deals_from_category_page
-      deals_html_doc = Nokogiri::HTML(open("https://www.goodreads.com/ebook-deals/today-only-book-deals"))
+    def scrape_deals_from_category_page(category)
+      deals_html_doc = Nokogiri::HTML(open(category.url))
       deals_html_elements = deals_html_doc.css(CATEGORY_PAGE_HTML_ELEMENTS)
 
-      deals_html_elements.map do |html_element|
-        {
-          title: html_element.css(DEALS_HTML_ELEMENT_TITLE).text,
-          author: html_element.css(DEALS_HTML_ELEMENT_AUTHOR).text,
-          categories: html_element.css(DEALS_HTML_ELEMENT_CATEGORIES).text,
-          description: html_element.css(DEALS_HTML_ELEMENT_DESCRIPTION).text,
-          deal_price: html_element.css(DEALS_HTML_ELEMENT_DEAL_PRICE).text,
-          original_price: html_element.css(DEALS_HTML_ELEMENT_ORIGINAL_PRICE).text,
-          deal_expiration_datetime: html_element.css(DEALS_HTML_ELEMENT_DATETIME).text.split(" ").drop(1)
-        }
+      deals_html_elements.each do |html_element|
+          deal = Deal.new
+          book = Book.new(deal)
+          book.title = html_element.css(DEALS_HTML_ELEMENT_TITLE).text
+          category.add_book(book)
+          book.author = html_element.css(DEALS_HTML_ELEMENT_AUTHOR).text
+          book.description = html_element.css(DEALS_HTML_ELEMENT_DESCRIPTION).text
+
+          deal.price = html_element.css(DEALS_HTML_ELEMENT_DEAL_PRICE).text
+          deal.original_price = html_element.css(DEALS_HTML_ELEMENT_ORIGINAL_PRICE).text
+          deal.expires_in = html_element.css(DEALS_HTML_ELEMENT_DATETIME).text.split(" ").drop(1).join(" ")
       end
     end
   end
