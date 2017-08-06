2a986b6e762c91221362ba622c0b5a07743eb10f
Soumya Veer <veer.soumya@gmail.com>
2017-08-05 21:16:13 -0700

    add required paths to book_deals.rb

--- lib/book_deals.rb
+++ lib/book_deals.rb
@@ -1,3 +1,7 @@
 require "book_deals/version"
 require_relative './book_deals/cli'
 require_relative './book_deals/launcher'
+require_relative './book_deals/scraper'
+require_relative './book_deals/category'
+require_relative './book_deals/book'
+require_relative './book_deals/deal'
