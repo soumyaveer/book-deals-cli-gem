a0caed505c9dc776f32512ae798852cc2995ddaf
Soumya Veer <veer.soumya@gmail.com>
2017-08-06 20:18:12 -0700

    alphabetize methods in Category

--- lib/book_deals/category.rb
+++ lib/book_deals/category.rb
@@ -2,14 +2,14 @@ module BookDeals
   class Category
     attr_accessor :name, :url, :books
 
-    def initialize(name)
-      self.name = name
-      self.books = []
-    end
-
     def add_book(book)
       self.books << book
       book.categories << self
+    end
+    
+    def initialize(name)
+      self.name = name
+      self.books = []
     end
   end
 end
