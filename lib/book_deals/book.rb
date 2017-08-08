d7b7da04574cdf850ca98453d833364acb8f07e5
Soumya Veer <veer.soumya@gmail.com>
2017-08-08 13:04:20 -0700

    fix initialize method for Book

--- lib/book_deals/book.rb
+++ lib/book_deals/book.rb
@@ -2,7 +2,10 @@ module BookDeals
   class Book
     attr_accessor :title, :author, :description, :deal, :categories
 
-    def initialize(deal)
+    def initialize(deal, title, author, description = nil)
+      self.title = title
+      self.author = author
+      self.description = description
       self.categories = []
       self.deal = deal
     end
