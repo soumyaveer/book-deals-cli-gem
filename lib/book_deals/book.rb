dbb8f0b6e24114370bfac3b733025b90a47e236c
Soumya Veer <veer.soumya@gmail.com>
2017-08-09 16:12:37 -0700

    display purchase url in Book#to_s

--- lib/book_deals/book.rb
+++ lib/book_deals/book.rb
@@ -4,7 +4,7 @@ module BookDeals
   class Book
     attr_accessor :title, :author, :description, :deal, :purchase_url, :categories
 
-    def initialize(deal, title, author, purchase_url description = nil)
+    def initialize(deal, title, author, purchase_url, description = nil)
       self.title = title
       self.author = author
       self.purchase_url = purchase_url
@@ -21,6 +21,7 @@ module BookDeals
       #{"Deal Price:".colorize(:yellow)} #{self.deal.price} \n
       #{"Original Price:".colorize(:yellow)} #{self.deal.original_price} \n
       #{"Expires in:".colorize(:yellow)} #{self.deal.expires_in} \n
+      #{"This book can be purchased on:".colorize(:yellow)} #{self.purchase_url.colorize(:magenta)} \n
       ==============================================================================
       DETAILS
     end
