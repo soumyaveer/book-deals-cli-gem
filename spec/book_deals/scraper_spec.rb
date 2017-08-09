5c6eff15c6659badadd84d824c86d967b09cac04
Soumya Veer <veer.soumya@gmail.com>
2017-08-09 13:07:32 -0700

    add tests for Scraper#scrape_deals_from_category_page and refactor Scraper#scrape_categories_from_home_page.

--- spec/book_deals/scraper_spec.rb
+++ spec/book_deals/scraper_spec.rb
@@ -12,26 +12,30 @@ describe BookDeals::Scraper do
   end
 
   describe "scrape_categories_from_home_page" do
-    it "scrapes all categories from the deals page" do
-      categories = scraper.scrape_categories_from_home_page
+    before do
+      @categories = scraper.scrape_categories_from_home_page
+    end
 
-      expect(categories.count).to eql(8)
+    it "scrapes all categories from the deals page" do
+      # categories = scraper.scrape_categories_from_home_page
+
+      expect(@categories.count).to eql(8)
     end
 
     it "scrapes all the categories names" do
-      categories_names = []
-      categories = scraper.scrape_categories_from_home_page
-      categories.each {|category| categories_names << category.name}
+      category_names = []
+      # categories = scraper.scrape_categories_from_home_page
+      @categories.each {|category| category_names << category.name}
 
-      expect(categories_names).to match_array(["Today Only!", "Bestsellers", "Romance", "Mystery & Thrillers", "Fantasy & Science Fiction", "Fiction", "Nonfiction", "Young Adult" ])
+      expect(category_names).to match_array(["Today Only!", "Bestsellers", "Romance", "Mystery & Thrillers", "Fantasy & Science Fiction", "Fiction", "Nonfiction", "Young Adult" ])
     end
 
     it "scrapes all the category urls" do
-      categories_urls = []
-      categories = scraper.scrape_categories_from_home_page
-      categories.each {|category| categories_urls << category.url}
+      category_urls = []
+      # categories = scraper.scrape_categories_from_home_page
+      @categories.each {|category| category_urls << category.url}
 
-      expect(categories_urls).to match_array(["https://www.goodreads.com/ebook-deals/today-only-book-deals",
+      expect(category_urls).to match_array(["https://www.goodreads.com/ebook-deals/today-only-book-deals",
                                              "https://www.goodreads.com/ebook-deals/bestsellers-book-deals",
                                              "https://www.goodreads.com/ebook-deals/romance-book-deals",
                                              "https://www.goodreads.com/ebook-deals/mystery-thrillers-book-deals",
@@ -39,6 +43,36 @@ describe BookDeals::Scraper do
                                              "https://www.goodreads.com/ebook-deals/fiction-book-deals",
                                              "https://www.goodreads.com/ebook-deals/nonfiction-book-deals",
                                               "https://www.goodreads.com/ebook-deals/young-adult-book-deals"])
+    end
+  end
+
+  describe "scrape_deals_from_category_page" do
+    before do
+      @category = BookDeals::Category.new("Bestsellers", "https://www.goodreads.com/ebook-deals/bestsellers-book-deals" )
+      @selected_category = scraper.scrape_deals_from_category_page(@category)
+
+    end
+
+    it "scrapes all book deals from the category requested" do
+      expect(@selected_category.books.count).to eql(12)
+    end
+
+    it "scrapes all books from category url" do
+      book_titles = []
+      @selected_category.books.each {|book| book_titles << book.title}
+      puts book_titles
+      expect(book_titles).to match_array(["Six of Crows (Six of Crows, #1)",
+                                         "Disclosure",
+                                         "Big Stone Gap (Big Stone Gap, #1)",
+                                         "Beneath a Scarlet Sky",
+                                         "A Land More Kind Than Home",
+                                         "Becoming Nicole: The Transformation of an American Family",
+                                         "Shadowshaper",
+                                         "Ride the Wind",
+                                         "All That Is Bitter And Sweet: A Memoir",
+                                         "For the Thrill of It",
+                                         "Blindspot: Hidden Biases of Good People",
+                                         "Battle for Atlantis (Atlantis, #6)"])
     end
   end
 end
