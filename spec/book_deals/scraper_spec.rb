7a5495ae180201d32345c8b4efad094ede473e7d
Soumya Veer <veer.soumya@gmail.com>
2017-08-09 13:23:23 -0700

    add test for Scraper#scrape_deals_from_category_page to check correct deal is scraped for the book.

--- spec/book_deals/scraper_spec.rb
+++ spec/book_deals/scraper_spec.rb
@@ -36,13 +36,13 @@ describe BookDeals::Scraper do
       @categories.each {|category| category_urls << category.url}
 
       expect(category_urls).to match_array(["https://www.goodreads.com/ebook-deals/today-only-book-deals",
-                                             "https://www.goodreads.com/ebook-deals/bestsellers-book-deals",
-                                             "https://www.goodreads.com/ebook-deals/romance-book-deals",
-                                             "https://www.goodreads.com/ebook-deals/mystery-thrillers-book-deals",
-                                             "https://www.goodreads.com/ebook-deals/fantasy-science-fiction-book-deals",
-                                             "https://www.goodreads.com/ebook-deals/fiction-book-deals",
-                                             "https://www.goodreads.com/ebook-deals/nonfiction-book-deals",
-                                              "https://www.goodreads.com/ebook-deals/young-adult-book-deals"])
+                                            "https://www.goodreads.com/ebook-deals/bestsellers-book-deals",
+                                            "https://www.goodreads.com/ebook-deals/romance-book-deals",
+                                            "https://www.goodreads.com/ebook-deals/mystery-thrillers-book-deals",
+                                            "https://www.goodreads.com/ebook-deals/fantasy-science-fiction-book-deals",
+                                            "https://www.goodreads.com/ebook-deals/fiction-book-deals",
+                                            "https://www.goodreads.com/ebook-deals/nonfiction-book-deals",
+                                            "https://www.goodreads.com/ebook-deals/young-adult-book-deals"])
     end
   end
 
@@ -60,19 +60,45 @@ describe BookDeals::Scraper do
     it "scrapes all books from category url" do
       book_titles = []
       @selected_category.books.each {|book| book_titles << book.title}
-      puts book_titles
+
       expect(book_titles).to match_array(["Six of Crows (Six of Crows, #1)",
-                                         "Disclosure",
-                                         "Big Stone Gap (Big Stone Gap, #1)",
-                                         "Beneath a Scarlet Sky",
-                                         "A Land More Kind Than Home",
-                                         "Becoming Nicole: The Transformation of an American Family",
-                                         "Shadowshaper",
-                                         "Ride the Wind",
-                                         "All That Is Bitter And Sweet: A Memoir",
-                                         "For the Thrill of It",
-                                         "Blindspot: Hidden Biases of Good People",
-                                         "Battle for Atlantis (Atlantis, #6)"])
+                                          "Disclosure",
+                                          "Big Stone Gap (Big Stone Gap, #1)",
+                                          "Beneath a Scarlet Sky",
+                                          "A Land More Kind Than Home",
+                                          "Becoming Nicole: The Transformation of an American Family",
+                                          "Shadowshaper",
+                                          "Ride the Wind",
+                                          "All That Is Bitter And Sweet: A Memoir",
+                                          "For the Thrill of It",
+                                          "Blindspot: Hidden Biases of Good People",
+                                          "Battle for Atlantis (Atlantis, #6)"])
+    end
+
+    it "scrapes all deals on the books" do
+      deals = []
+
+      @selected_category.books.each do |book|
+        deals <<  {
+          title: book.title,
+          author: book.author,
+          deal_price: book.deal.price
+        }
+      end
+
+      expect(deals.count).to eql(12)
+      expect(deals).to match_array([{:title=>"Six of Crows (Six of Crows, #1)", :author=>"Leigh Bardugo", :deal_price=>"$2.99"},
+                                    {:title=>"Disclosure", :author=>"Michael Crichton", :deal_price=>"$1.99"},
+                                    {:title=>"Big Stone Gap (Big Stone Gap, #1)", :author=>"Adriana Trigiani", :deal_price=>"$1.99"},
+                                    {:title=>"Beneath a Scarlet Sky", :author=>"Mark T. Sullivan", :deal_price=>"$1.99"},
+                                    {:title=>"A Land More Kind Than Home", :author=>"Wiley Cash", :deal_price=>"$1.99"},
+                                    {:title=>"Becoming Nicole: The Transformation of an American Family", :author=>"Amy Ellis Nutt", :deal_price=>"$1.99"},
+                                    {:title=>"Shadowshaper", :author=>"Daniel JosÃ© Older", :deal_price=>"$1.99"},
+                                    {:title=>"Ride the Wind", :author=>"Lucia St. Clair Robson", :deal_price=>"$1.99"},
+                                    {:title=>"All That Is Bitter And Sweet: A Memoir", :author=>"Ashley Judd", :deal_price=>"$1.99"},
+                                    {:title=>"For the Thrill of It", :author=>"Simon Baatz", :deal_price=>"$1.99"},
+                                    {:title=>"Blindspot: Hidden Biases of Good People", :author=>"Mahzarin R. Banaji", :deal_price=>"$1.99"},
+                                    {:title=>"Battle for Atlantis (Atlantis, #6)", :author=>"Bob Mayer", :deal_price=>"$1.99"}])
     end
   end
 end
